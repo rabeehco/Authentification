82dbc47cfc3b49ffbd99a1684584a6e5e475bf86
codewithrabeeh <real.ray48@gmail.com>
2022-06-17 16:16:10 +0530

    bcrypt

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const bcrypt = require('bcrypt')
 
-const hashPass = async() => {
-    const salt = await bcrypt.genSalt(10)
-    console.log(salt)
+// const hashPass = async(pw) => {
+//     const salt = await bcrypt.genSalt(10)
+//     const hash = await bcrypt.hash(pw, salt)
+//     console.log(salt)
+//     console.log(hash)
+// }
+
+const hashPass = async(pw) => {
+    const hash = await bcrypt.hash(pw, 12)
+    // console.log(salt)
+    console.log(hash)
 }
 
-hashPass()
+const login = async (pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw)
+    if(result){
+        console.log('Pass Matched')
+    } else {
+        console.log('Pass Not Matched!!!')
+    }
+}
+
+// hashPass('monkey')
+login('monkey', '$2b$12$/uBNVEahSMbq4RxXzWtSYupetRdLtHQxTdZgzKNJDvzPDQkO6VZBO')
