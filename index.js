dd085477f27fbc6adede334b06fdc2a790105841
codewithrabeeh <real.ray48@gmail.com>
2022-06-18 10:03:19 +0530

    added login middleware

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.set('views', 'views')
 app.use(express.urlencoded({extended: true}))
 app.use(session({secret: 'notagoodsecret'}))
 
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id){
+        return res.redirect('/login')
+    }
+        next();
+    }
+
 app.get('/', (req, res) => {
     res.send('Reached Home Page')
 })
@@ -63,13 +70,8 @@ app.get('/login', (req, res) => {
     
 })
 
-app.get('/secret', (req, res) => {
-    if(req.session.user_id){
+app.get('/secret', requireLogin, (req, res) => {
         res.render('secret')
-    } else {
-        res.redirect('/login')
-    }
-    
 })
 
 
